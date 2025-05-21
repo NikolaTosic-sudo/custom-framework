a44c94e09476068056dadf0b35010c0d6e79d4bb
Nikola Tosic <toleslaoffice@gmail.com>
2025-05-20 17:05:54 -0700

    restart the dom builder

--- plugins/template-plugin.js
+++ plugins/template-plugin.js
@@ -28,94 +28,13 @@ module.exports = function templatePlugin() {
           const jsxElement = templateNode.expression;
 
           // recursive function
-          // is there a need for recursive function?
-          // maybe just change the buildDom function
-          // it's time to remove the htmlparser2 package
+          // time to build the dom from the ground
 
-          const domAst = buildDomAST(dom.children);
+          // const domAst = buildDomAST(jsxElement.children);
 
-          renderMethod.body.body = [t.returnStatement(domAst)];
+          // renderMethod.body.body = [t.returnStatement(domAst)];
         }
       }
     }
   };
 };
-
-function buildDomAST(nodes) {
-  const t = require('@babel/types');
-
-  function createNode(node) {
-    if (node.type === 'text') {
-      const interpolated = node.data.replace(/\{\{(.+?)\}\}/g, (_, expr) => `\${${expr.trim()}}`);
-      return t.callExpression(
-        t.memberExpression(t.identifier('document'), t.identifier('createTextNode')),
-        [t.templateLiteral([t.templateElement({ raw: interpolated, cooked: interpolated }, true)], [])]
-      );
-    }
-
-    if (node.type === 'tag') {
-      const create = t.callExpression(
-        t.memberExpression(t.identifier('document'), t.identifier('createElement')),
-        [t.stringLiteral(node.name)]
-      );
-
-      const elVar = t.identifier('el');
-      const elDecl = t.variableDeclaration('const', [
-        t.variableDeclarator(elVar, create)
-      ]);
-
-      const children = (node.children || []).map(createNode);
-      const appends = children.map(child =>
-        t.expressionStatement(
-          t.callExpression(
-            t.memberExpression(elVar, t.identifier('appendChild')),
-            [child]
-          )
-        )
-      );
-
-      return t.callExpression(
-        t.arrowFunctionExpression([], t.blockStatement([
-          elDecl,
-          ...appends,
-          t.returnStatement(elVar)
-        ])),
-        []
-      );
-    }
-
-    return t.nullLiteral();
-  }
-
-  const roots = nodes.map(createNode);
-
-  if (roots.length === 1) return roots[0];
-
-  const frag = t.callExpression(
-    t.memberExpression(t.identifier('document'), t.identifier('createDocumentFragment')),
-    []
-  );
-
-  const fragVar = t.identifier('frag');
-  const fragDecl = t.variableDeclaration('const', [
-    t.variableDeclarator(fragVar, frag)
-  ]);
-
-  const appendAll = roots.map(child =>
-    t.expressionStatement(
-      t.callExpression(
-        t.memberExpression(fragVar, t.identifier('appendChild')),
-        [child]
-      )
-    )
-  );
-
-  return t.callExpression(
-    t.arrowFunctionExpression([], t.blockStatement([
-      fragDecl,
-      ...appendAll,
-      t.returnStatement(fragVar)
-    ])),
-    []
-  );
-}
